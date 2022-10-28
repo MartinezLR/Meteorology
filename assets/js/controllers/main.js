09e17ee05c5274dcacc9f7aa151067055d39d1a7
Lucas Martinez <Lucas.martinez799@gmail.com>
2022-10-28 06:30:10 -0300

    Remake - JavaScript

--- assets/js/controllers/main.js
+++ assets/js/controllers/main.js
@@ -1,4 +1,4 @@
-app.controller('mainCtrl', function ($scope, $rootScope, request) {
+app.controller('ctrl.main', ['$scope', '$rootScope', '$request', function (scope, rootScope, request) {
 
     const fadeOut = () => {
         const loaderWrapper =
@@ -8,139 +8,110 @@ app.controller('mainCtrl', function ($scope, $rootScope, request) {
 
     window.addEventListener('load', fadeOut);
 
-    $scope.translation = {
+    scope.requests = {
 
-        'clear sky': 'Céu limpo',
-        'few clouds': 'Poucas nuvens',
-        'scattered clouds': 'Nuvens dispersas',
-        'broken clouds': 'Nuvens quebradas',
-        'overcast clouds': 'Nuvens nubladas',
-        'shower rain': 'Chuva',
-        'rain': 'Raios',
-        'snow': 'Neve',
-        'mist': 'Névoa',
+        // Requisição do localizador de região com base na cidade
+        location: function (local) {
+            if (local == undefined) return;
+            request.getLocation(local).then((response) => scope.loader(response.data))
+        },
 
-        // Thunderstorm = Tempestade (Grupo)
-        'thunderstorm with light rain': 'Trovoada com chuva fraca',
-        'thunderstorm with rain': 'Trovoada com chuva',
-        'thunderstorm with heavy rain': 'Trovoada com chuva forte',
-        'light thunderstorm': 'Trovoada leve',
-        'thunderstorm': 'Tempestade',
-        'heavy thunderstorm': 'Forte tempestade',
-        'ragged thunderstorm': 'Tempestade irregular',
-        'thunderstorm with light drizzle': 'Trovoada com chuvisco',
-        'thunderstorm with drizzle': 'Trovoada com chuvisco',
-        'thunderstorm with heavy drizzle': 'Trovoada com chuva forte',
+        // Requisição do localizador de região com base no seu local atual
+        geolacation: function (lat, log) {
+            request.getGeolacation(lat, log).then((response) => scope.loader(response.data))
+        },
 
-        // Drizzle = Chuvisco (Grupo)
-        'light intensity drizzle': 'Chuvisco de intensidade de luz',
-        'drizzle': 'Chuvisco',
-        'heavy intensity drizzle': 'Chuvisco de forte intensidade',
-        'light intensity drizzle rain': 'Chuva de intensidade leve com raios',
-        'drizzle rain': 'Chuvisco com raios',
-        'heavy intensity drizzle rain': 'Chuva de garoa de forte intensidade',
-        'shower rain and drizzle': 'Chuva e chuvisco',
-        'heavy shower rain and drizzle': 'Chuva forte chuva e chuvisco',
-        'shower drizzle': 'Chuvisco',
-
-        // Rain = Raios (Grupo)
-        'light rain': 'Chuva leve',
-        'moderate rain': 'Chuva moderada',
-        'heavy intensity rain': 'Chuva de forte intensidade',
-        'very heavy rain': 'Chuva muito forte',
-        'extreme rain': 'Chuva extrema',
-        'freezing rain': 'Chuva congelante',
-        'light intensity shower rain': 'Chuva de intensidade de luz',
-        'shower rain': 'Chuva de banho',
-        'heavy intensity shower rain': 'Chuva de chuva de forte intensidade',
-        'ragged shower rain': 'Chuva de chuveiro irregular',
-
-        // Snow = Neve (Grupo)
-        'light snow': 'Pouca neve',
-        'Snow': 'Neve',
-        'Heavy snow': 'Neve pesada',
-        'Sleet': 'granizo',
-        'Light shower sleet': 'Chuveiro leve',
-        'Shower sleet': 'Chuveiro de granizo',
-        'Light rain and snow': 'Chuva leve e neve',
-        'Rain and snow': 'Chuva e neve',
-        'Light shower snow': 'Chuva de neve leve',
-        'Shower snow': 'Chuva de neve',
-        'Heavy shower snow': 'Chuva de neve forte',
-
-        // Clear = Limpo (Grupo)
-        'clear sky': 'Céu limpo',
-
-        // Clouds = Nuvens (Grupo)
-        'few clouds: 11-25%': 'Poucas nuvens: 11-25%',
-        'scattered clouds: 25-50%': 'Nuvens espalhadas: 25-50%',
-        'broken clouds: 51-84%': 'Nuvens quebradas: 51-84%',
-        'overcast clouds: 85-100%': 'Nuvens nubladas: 85-100%',
+        // Requisição do Dicionario
+        translation: function () {
+            request.getTranslation().then((response) => response.data);
+        }
     }
 
-    $scope.send = function (local) {
+    scope.loader = function (response) {
 
-        local == ''
+        scope.weather = {
 
-            ? $scope.location()
-
-            : request.getLocation(local).then((data) => $scope.load(data));
-    }
-
-    $scope.geolocation = function (lat, log) {
-
-        request.getGeolacation(lat, log).then((data) => $scope.load(data));
-    }
-
-    $scope.load = function (data) {
-
-        $rootScope.weather = {
-
-            full: data,
-
-            local: data.name,
-
-            icone: `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-
-            temperatura: `${Math.floor(data.main.temp)}°`,
-
-            umidade: `${data.main.humidity}%`,
-
-            velocidade: `${Math.floor(data.wind.speed)}km/h`,
-
-            nublado: `${data.clouds.all}%`,
-
-            clima: function () {
-
-                $.each($scope.translation, function (indice, value) {
-
-                    if (indice == data.weather[0].description) $scope.clima = value
-                });
-
-                return $scope.clima
+            locate: function () {
+                let name = response.name
+                return name
             },
 
-            horas: function () {
+            icon: function () {
+                let icone = `http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+                return icone
+            },
 
-                const date = new Date()
+            temperature: function () {
+                let temperature = `${Math.floor(response.main.temp)}°`
+                return temperature
+            },
 
-                var hours = date.getHours();
-                var minutes = date.getMinutes();
-                var ampm = hours >= 12 ? 'PM' : 'AM';
-                hours = hours % 12;
-                hours = hours ? hours : 12; // the hour '0' should be '12'
-                minutes = minutes < 10 ? '0' + minutes : minutes;
-                var strTime = hours + ':' + minutes + ' ' + ampm;
-                return strTime;
+            moisture: function () {
+                let moisture = `${response.main.humidity}%`
+                return moisture
+            },
+
+            velocity: function () {
+                let velocity = `${Math.floor(response.wind.speed)}km/h`
+                return velocity
+            },
+
+            cloudy: function () {
+                let cloudy = `${response.clouds.all}%`
+                return cloudy
+            },
+
+            climate: function () {
+                let climate = response.weather[0].description
+                return climate
             }
         }
-
-        console.log($rootScope.weather.full);
     }
 
-    $scope.location = function () {
-        navigator.geolocation.getCurrentPosition(p => $scope.geolocation(p.coords.latitude, p.coords.longitude))
-    }
+    scope.utc = {
 
-    $scope.location();
-})
+        date: new Date(),
+
+        day: function () {
+            let day = scope.utc.date.getDate();
+            return day < 10 ? `0${day}` : day;
+        },
+
+        month: function () {
+            let month = scope.utc.date.getMonth() + 1;
+            return month < 10 ? `0${month}` : month;
+        },
+
+        year: function () {
+            let year = scope.utc.date.getFullYear();
+            return year
+        },
+
+        completeDate: function () {
+            date = `${scope.utc.day()}/${scope.utc.month()}/${scope.utc.year()}`
+            return date
+        },
+
+        hours: function () {
+            let hours = scope.utc.date.getHours();
+            hours = hours % 12;
+            hours = hours ? hours : 12;
+            return hours
+        },
+
+        minutes: function () {
+            let minutes = scope.utc.date.getMinutes();
+            return minutes < 10 ? `0${minutes}` : minutes;
+        },
+
+        periods: function () {
+            let ampm = scope.utc.date.getHours() >= 12 ? 'PM' : 'AM';
+            return ampm
+        },
+
+        completeTime: function () {
+            let time = `${scope.utc.hours()}:${scope.utc.minutes()} ${scope.utc.periods()}`
+            return time;
+        },
+    }
+}])
--- assets/js/factorys/request.js
+++ assets/js/factorys/request.js
@@ -1,27 +1,20 @@
-app.factory('request', ['$http', function ($http) {
+app.factory('$request', ['$http', function (http) {
 
-    var getLocation = function (location, key = '24cadfe23677b8512578d5f4d78e94e4') {
-
-        return $http.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`)
-
-            .then(function (response) {
-
-                return response.data
-            });
+    const getLocation = function (location, key = '24cadfe23677b8512578d5f4d78e94e4') {
+        return http.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=metric`);
     }
 
-    var getGeolacation = function (latitude, longitude, key = '24cadfe23677b8512578d5f4d78e94e4') {
+    const getGeolacation = function (latitude, longitude, key = '24cadfe23677b8512578d5f4d78e94e4') {
+        return http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`);
+    }
 
-        return $http.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`)
-
-            .then(function (response) {
-
-                return response.data
-            });
+    const getTranslation = function () {
+        return http.get('assets/json/translation.json');
     }
 
     return {
         getLocation: getLocation,
         getGeolacation: getGeolacation,
+        getTranslation: getTranslation,
     };
 }]);
