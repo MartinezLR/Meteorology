95dff80bed4322032393a917a0c9ac58cccbabc7
zZenitram <lucas.martinez799@gmail.com>
2024-07-16 01:26:08 -0300

    refactor: removido arquivos descontinuados

--- src/assets/scripts/config/router/route.js
+++
@@ -1,8 +0,0 @@
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/inicio', {
-            templateUrl: 'src/pages/inicio/inicio.html',
-            controller: '$ctrl.inicio',
-        })
-        .otherwise({ redirectTo: '/inicio' });
-}])
