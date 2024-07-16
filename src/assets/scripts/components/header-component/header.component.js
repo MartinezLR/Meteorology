9376853df4e76a91252ba1eb9f5a492b1d5bfe81
zZenitram <lucas.martinez799@gmail.com>
2024-07-16 03:15:15 -0300

    feat: data

--- src/assets/scripts/components/header-component/header.component.js
+++ src/assets/scripts/components/header-component/header.component.js
@@ -1,8 +1,12 @@
 angular.module('rootModule').component('headerComponent', {
     templateUrl: 'src/assets/scripts/components/header-component/header.component.html',
     controller: headerController,
+    controllerAs: 'ctrl'
 });
 
-function headerController() {
+// Start the refresh time interval when the component is initialized
+headerController.$inject = ['dateTimeService'];
 
+function headerController(dateTimeService) {
+    this.completeDateDate = dateTimeService.getCompleteDate();
 }
