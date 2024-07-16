451b4ceeab26f5214d6bc3c5e1cb53b14f93efdd
zZenitram <lucas.martinez799@gmail.com>
2024-07-16 03:15:07 -0300

    feat: relógio

--- src/assets/scripts/components/timer-component/timer.component.js
+++ src/assets/scripts/components/timer-component/timer.component.js
@@ -1,8 +1,12 @@
 angular.module('rootModule').component('timerComponent', {
     templateUrl: 'src/assets/scripts/components/timer-component/timer.component.html',
     controller: timerController,
+    controllerAs: 'ctrl'
 });
 
-function timerController() {
+// Start the refresh time interval when the component is initialized
+timerController.$inject = ['dateTimeService'];
 
+function timerController(dateTimeService) {
+    this.completeTime = dateTimeService.getCompleteTime();
 }
